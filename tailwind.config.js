72129cde1c07be5512a6cd7a3998a33d07ffd5da
A.I <merrick@luois.me>
2022-01-10 10:31:41 +0800

    introduce tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: ["./pages/**/*.jsx", "./components/**/*.jsx"],
+  theme: {
+    extend: {
+      flex: {
+        "l-24": "0 0 6rem",
+      },
+      fontFamily: {
+        sans: ["overpass"],
+        serif: ["Arvo"],
+        mono: ["Source Code Pro"],
+      },
+      padding: {
+        "1/2": "0.125rem",
+      },
+      colors: {
+        background: "var(--color-background)",
+        "background-alt": "var(--color-background-alt)",
+        "background-dark": "var(--color-background-dark)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        black: "var(--color-black)",
+        red: "var(--color-red)",
+        green: "var(--color-green)",
+        yellow: "var(--color-yellow)",
+        blue: "var(--color-blue)",
+        magenta: "var(--color-magenta)",
+        cyan: "var(--color-cyan)",
+        white: "var(--color-white)",
+      },
+      screens: {
+        "dark-mode": { raw: "(prefers-color-scheme: dark)" },
+      },
+    },
+  },
+  plugins: [],
+};
