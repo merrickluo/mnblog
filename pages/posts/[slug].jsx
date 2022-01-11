8895fd27d5616bf64ff1cb8e0974885443e0acf6
A.I <merrick@luois.me>
2022-01-11 14:10:10 +0800

    add og for articles and post index page

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Tag from "../../components/tag";
+import { useRouter } from "next/router";
 
 import { getPostData, getSortedPostsData } from "../../lib/posts";
 
@@ -22,10 +23,21 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Post({ postData }) {
+  const { asPath } = useRouter();
+  const permanentUrl = `${process.env.baseUrl}${asPath}`;
   return (
     <>
       <Head>
         <title>{postData.title} - A.I.&#39;s Blog</title>
+        <meta name="author" content="A.I." />
+        <meta property="og:locale" content={postData.language} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={postData.title} />
+        <meta property="og:description" content={postData.description} />
+        <meta property="og:url" content={permanentUrl} />
+        <meta property="og:site_name" content="A.I.&#39;s Blog" />
+        <meta property="og:image" content="" /> {/* TODO add avatar */}
+        <meta name="description" content={postData.description} />
       </Head>
       <section className="mb-6">
         <h1 className="text-4xl font-bold">{postData.title}</h1>
---
+++ pages/posts/index.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import PostList from "../../components/post_list";
+import { getSortedPostsData } from "../../lib/posts";
+
+export function getStaticProps() {
+  return {
+    props: {
+      posts: getSortedPostsData(),
+    },
+  };
+}
+
+export default function Posts({ posts }) {
+  return (
+    <>
+      <Head>
+        <title>All posts - A.I.&#39;s Blog</title>
+        <meta name="description" content="A.I. Writes here." />
+      </Head>
+      <h2 className="text-3xl font-bold mb-4">Al Posts:</h2>
+      <PostList posts={posts} />
+    </>
+  );
+}
