a1aed436004e45d67a3f383f3864bc24c37d9e38
A.I <merrick@luois.me>
2022-09-17 19:10:31 +0800

    update the site description

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     baseUrl: "https://merrick.luois.me",
     title: "Node 13",
-    description: "The 13th node of …",
+    description:
+      "Just a random node on the internet, assigned with my favorite number.",
   },
 };
