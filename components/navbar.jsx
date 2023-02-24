478a4f6a4ea03b8aa2771ffb6d17481cc191a156
Merrick Luo <merrick@luois.me>
2023-02-24 22:36:30 +0800

    use icon for rss and email

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import { HiRss, HiMail } from "react-icons/hi";
 
 export default function Navbar() {
-  return <>
-    {/* normal menu */}
-    <div className="flex">
-      <Link href="/posts" className="text-xl mx-2">
-        /Posts
-      </Link>
-      <Link href="/posts/feed.xml" className="text-xl mx-2">
-        /RSS
-      </Link>
-    </div>
-  </>;
+  return (
+    <>
+      {/* normal menu */}
+      <div className="flex items-center justify-center">
+        <Link href="mailto:merrick@luois.me" className="mx-2 text-2xl icon">
+          <HiMail />
+        </Link>
+        <Link href="/posts/feed.xml" className="mx-2 text-2xl icon">
+          <HiRss />
+        </Link>
+      </div>
+    </>
+  );
 }
