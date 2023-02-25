44d2a280f74706738b71c0a822eb582ba3c81043
Merrick Luo <merrick@luois.me>
2023-02-25 09:32:40 +0800

    fix a title warning

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -26,41 +26,45 @@ export function getStaticProps({ params }) {
 export default function Post({ postData }) {
   const { asPath } = useRouter();
   const permanentUrl = `${process.env.baseUrl}${asPath}`;
-  return <>
-    <Head>
-      <title>
-        {postData.title} - {process.env.title}
-      </title>
-      <meta name="author" content={postData.author || "A.I."} />
-      <meta property="og:locale" content={postData.language} />
-      <meta property="og:type" content="article" />
-      <meta property="og:title" content={postData.title} />
-      <meta property="og:description" content={postData.description} />
-      <meta property="og:url" content={permanentUrl} />
-      <meta property="og:site_name" content={process.env.title} />
-      <meta property="og:image" content="" /> {/* TODO add avatar */}
-      <meta name="description" content={postData.description} />
-    </Head>
-    <section className="mb-6">
-      <h1 className="text-4xl font-bold">{postData.title}</h1>
-      <time dateTime={postData.date}>
-        {new Date(postData.date).toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })}
-      </time>
-      <ol className="mt-4">
-        {postData.categories &&
-          postData.categories.map((c) => (
-            <li key={c} className="inline-block mr-2 text-sm">
-              <Link href={`/categories/${encodeURIComponent(c)}`} passHref legacyBehavior>
-                <Tag>{c}</Tag>
-              </Link>
-            </li>
-          ))}
-      </ol>
-    </section>
-    <article dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-  </>;
+  return (
+    <>
+      <Head>
+        <title>{`${postData.title} - ${process.env.title}`}</title>
+        <meta name="author" content={postData.author || "A.I."} />
+        <meta property="og:locale" content={postData.language} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={postData.title} />
+        <meta property="og:description" content={postData.description} />
+        <meta property="og:url" content={permanentUrl} />
+        <meta property="og:site_name" content={process.env.title} />
+        <meta property="og:image" content="" /> {/* TODO add avatar */}
+        <meta name="description" content={postData.description} />
+      </Head>
+      <section className="mb-6">
+        <h1 className="text-4xl font-bold">{postData.title}</h1>
+        <time dateTime={postData.date}>
+          {new Date(postData.date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+        </time>
+        <ol className="mt-4">
+          {postData.categories &&
+            postData.categories.map((c) => (
+              <li key={c} className="inline-block mr-2 text-sm">
+                <Link
+                  href={`/categories/${encodeURIComponent(c)}`}
+                  passHref
+                  legacyBehavior
+                >
+                  <Tag>{c}</Tag>
+                </Link>
+              </li>
+            ))}
+        </ol>
+      </section>
+      <article dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </>
+  );
 }
